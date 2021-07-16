685dde296cbb4da0e9d03c75d618f7bb51545dc4
aritra <aritrachakraborty0124@gmail.com>
2021-07-17 01:40:20 +0530

    initial test

--- app.py
+++ app.py
@@ -1,44 +1,24 @@
 from flask import Flask, request, render_template, send_file
-import s3_test, rds_test, ec2_test
 import json
-import terraform_generator_for_S3, terraform_generator_for_EC2, terraform_generator_for_RDS
-
+import pymysql
 
 app = Flask(__name__)
 
+def db():
+    db = pymysql.connect("localhost", "pmauser", "aritraroot", "tracker")
+    cursor = db.cursor()
+    sql = "select TIMEDIFF(%s, %s)"
+    cursor.execute(sql, (data["to_time"], data["from_time"]))
+    total_time = cursor.fetchone()
+    sql = "insert into activity_update values(0,%s, %s, NOW(), %s, %s, %s)"
+    id = cursor.execute(sql, (
+        session["activity_id"], session['id'], data["from_time"], data["to_time"],
+        total_time))
+    db.commit()
+    db.close()
+    # id = cursor.fetchone()
+    return id
 
-@app.route('/ec2', methods=['POST'])
-def create_ec2():
-    if request.method == 'POST':
-        data = json.loads(request.get_data(as_text=True))
-        response = ec2_test.create_ec2(data)
-        terraform_generator_for_EC2.terraform_ec2(data)
-        return {"status": "working", "response": response}
-
-
-@app.route('/s3', methods=['POST'])
-def create_s3():
-    if request.method == 'POST':
-        data = json.loads(request.get_data(as_text=True))
-        response = s3_test.create_s3(data)
-        
-        terraform_generator_for_S3.terraform_s3(data)
-        return {"status": "working", "response": response}
-
-
-@app.route('/rds', methods=['POST'])
-def create_rds():
-    if request.method == 'POST':
-        data = json.loads(request.get_data(as_text=True))
-        response = rds_test.create_rds(data)
-        terraform_generator_for_RDS.terraform_rds(data)
-        return {"status": "working", "response": response}
-
-@app.route('/download')
-def download_file():
-    print("here")
-    path = '/home/aritra/PycharmProjects/aws_instance_generator/terraform/config.tf.json'
-    return send_file(path, attachment_filename='config.tf.json',as_attachment=True)
 
 @app.route('/')
 def hello_world():
